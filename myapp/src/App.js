c4725523bc3444896a2349df52dedb43ba453c1e
micole11 <uchmic@gmail.com>
2023-07-07 18:06:39 +0100

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BasicExample from './form';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicExample /> 
+          
+    </div>
+  );
+}
+
+export default App;
